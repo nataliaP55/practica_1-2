25fef53760132cfa2b7e02b6fc421571c990e574
Natalia Pastrovicchio <nataliapastrovicchio@gmail.com>
2022-04-07 10:09:43 -0300

    agregar archivos

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+console.log("funciona");
+
+document.write('<iframe src="https://cdn.pixabay.com/audio/2022/03/23/audio_07b2a04be3.mp3" frameborder="0"></iframe>');
+
+document.write('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19116.905943267477!2d-58.3857790247466!3d-34.62393458980262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a334ccee26fc95%3A0x2edeeb69361bc460!2sParque%20Lezama!5e0!3m2!1ses-419!2sar!4v1649254136434!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>');
+
+document.write('<iframe src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>');
+
+document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/e-N2d5ED2KE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
